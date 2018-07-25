527e9db55d71887f9d1e6c1fd9cd960336ef4685
Axel Rasmussen <axel.rasmussen1@gmail.com>
2018-07-24 20:00:19 -0700

    Also apply fixed path discovery logic to coverage.sh.

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env bash
 
-for COMMAND in kcov pgrep; do
+for COMMAND in kcov pgrep readlink; do
     if ! command -v "$COMMAND" > /dev/null; then
         echo "Required command '$COMMAND' not found"
         exit 1
     fi
 done
 
-TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BASE_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
-cd "$BASE_DIR" || exit 1
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+PROJECT_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
+while [[ ! -f "$PROJECT_DIR/Cargo.toml" ]]; do
+    if [[ "$PROJECT_DIR" == "/" ]]; then
+        echo "Failed to locate project's Cargo.toml."
+        exit 1
+    fi
+    PROJECT_DIR="$(dirname "$PROJECT_DIR")"
+done
 
 CHROME=$(command -v google-chrome-stable || echo -n "/dev/null")
 FIREFOX=$(command -v firefox || echo -n "/dev/null")
 
-"$TOOLS_DIR/binaries.sh" test | while read -r BINARY_PATH; do
+"$SCRIPT_DIR/binaries.sh" test | while read -r BINARY_PATH; do
     BINARY="$(basename "$BINARY_PATH")"
-    COVERAGE_DIR="target/cov/$BINARY"
+    COVERAGE_DIR="$PROJECT_DIR/target/cov/$BINARY"
     COVERAGE_REPORT="$COVERAGE_DIR/index.html"
 
     mkdir -p "$COVERAGE_DIR" || exit 1
     kcov \
         --verify \
-        --include-path="$BASE_DIR/src" \
-        --exclude-path="$BASE_DIR/src/tests" \
+        --include-path="$PROJECT_DIR/src" \
+        --exclude-path="$PROJECT_DIR/src/tests" \
         "$COVERAGE_DIR" \
         "$BINARY_PATH" || exit 1
 
