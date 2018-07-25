1bbdf4e6187905c6020c374b02ca494a11b0ce19
Axel Rasmussen <axel.rasmussen1@gmail.com>
2018-07-24 19:55:52 -0700

    Also apply new path discovery logic to test.sh.

--- bin/test.sh
+++ bin/test.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-for COMMAND in find shellcheck; do
+for COMMAND in find readlink shellcheck; do
     if ! command -v "$COMMAND" > /dev/null; then
         echo "Required command '$COMMAND' not found"
         exit 1
     fi
 done
 
-BASE_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
-cd "$BASE_DIR" || exit 1
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+TEMPLATE_DIR="$(dirname "$SCRIPT_DIR")"
 
-find "$BASE_DIR" -type f -iname "*.sh" -print0 | while IFS= read -r -d $'\0' FILE; do
+find "$TEMPLATE_DIR" -type f -iname "*.sh" -print0 | while IFS= read -r -d $'\0' FILE; do
     echo "Testing '$FILE'..."
     shellcheck "$FILE"
 done
